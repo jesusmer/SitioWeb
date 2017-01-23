253c3740fc4ac7380c4e7e86bb0b26ddd67f14d8
jesus mercado <jesus mercado>
2017-01-23 12:11:29 -0700

    css+js+php+sql

---
+++ app.js
@@ -0,0 +1,8 @@
+alert('hola UDO');
+function login()
+{
+    var username = document.getElementById('txtidusuario').value;
+    var password = document.getElementById('txtPasswd').value;
+    var recorder = document.getElementById('remember').value;
+    alert('Usuario'+ username +', Cointrasena'+ password+', rcordar'+ recorder);
+}
