30c84632ae98c6ea4d03318c92a3d2b4df8cad27
jesus mercado <chuyb312@gmail.com>
2017-03-24 11:56:07 -0700

    24

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 alert('hola UDO');
 function login()
 {
+    //hola
     var username = document.getElementById('txtidusuario').value;
     var password = document.getElementById('txtPasswd').value;
     var recorder = document.getElementById('remember').value;
